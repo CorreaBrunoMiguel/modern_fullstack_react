9ac3e810ff9ff18d6c499b4c15c92bc52bb884ff
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-08-27 16:55:49 -0300

    build: finalizado ch2

---
+++ ch2/backend/files.js
@@ -0,0 +1,12 @@
+import { writeFileSync, readFileSync } from 'node:fs'
+
+const users = [{ name: 'Bruno Miguel', email: 'bruno@miguel.com' }]
+
+const usersJson = JSON.stringify(users)
+
+writeFileSync('backend/users.json', usersJson)
+
+const readUsersJson = readFileSync('backend/users.json')
+const readUsers = JSON.parse(readUsersJson)
+
+console.log(readUsers)
